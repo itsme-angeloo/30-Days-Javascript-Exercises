2ec9dd74100195f105d047cec51d18fa14b059f9
Angelo Loreno <angelogarcialoreno@gmail.com>
2025-05-26 01:26:03 +0800

    Added changes to Day01 Exercises

---
+++ 30-days-challenge/day01.js
@@ -0,0 +1,5 @@
+//DAY 1: Declare variables for your name, age, and favorite color. Display them using console.log().
+
+var name = "Angelo Loreno", age = 21, favColor = "Blue"
+
+console.log(`Hi ${name}, You're ${age} years old and ${favColor} is your favorite color!`)
