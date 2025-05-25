d2757372e6bdb021696620be322a5932a632d871
Angelo Loreno <angelogarcialoreno@gmail.com>
2025-05-26 01:35:10 +0800

    Updated Read.md

---
+++ 30-days-challenge/day02.js
@@ -0,0 +1,10 @@
+//Day 3:** Create two number variables and print their sum, difference, product, and quotient.
+
+var x = 3, y = 8, sum, diff, prod, quo
+
+sum = x + y
+diff = x - y
+prod = x * y
+quo = x / y
+
+console.log(`Sum: ${sum} \nDifference: ${diff} \nProduct ${prod} \nQuotient: ${quo}`)
